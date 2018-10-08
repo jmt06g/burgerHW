2677d59b8f6480d1f6776decaa8ed2db0cd76cfc
Jon Tapanes <jon@Jons-MacBook-Air.local>
2018-10-08 15:09:35 -0400

    seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (name, burger_name, devoured) VALUES ("The Big Pun", 90, "yes");
+INSERT INTO burger (name, burger_name, devoured) VALUES ("Money Mike Burger", 85, "yes");
+INSERT INTO burger (name, burger_name, devoured) VALUES ("Cheese", 70, "yes");
--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// MySQL DB Connection Information (remember to change this with our specific credentials)
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  //also check port
+  user: "root",
+  password: "",
+  database: "burger"
+  //if this doesnt work check to see if it's not "burgers"
+});
